bf991dbfc6cb375a22a70cb44be3e68a762d5635
jcheron <myaddressmail@gmail.com>
2018-03-20 17:22:32 +0100

    example

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -5,6 +5,7 @@ export default Route.extend({
   model(){
     return RSVP.hash({
       developers:this.get('store').findAll('developer',{include:"projects"}),
+      fields:['identity',{name:'identity',component:'lbl-value',caption:'Labeled identity'}],
       operations:[{icon:'red remove',link:'developers.delete'},{icon:'edit',link:'developers.update'}]
     });
   }
