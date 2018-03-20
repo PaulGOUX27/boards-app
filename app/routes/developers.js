438cd6aacaca6c6a699e3cf9d609e17e76b44cc9
jcheron <myaddressmail@gmail.com>
2018-03-20 17:22:13 +0100

    ui-table with components

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -5,7 +5,6 @@ export default Route.extend({
   model(){
     return RSVP.hash({
       developers:this.get('store').findAll('developer',{include:"projects"}),
-      fields:['identity'],
       operations:[{icon:'red remove',link:'developers.delete'},{icon:'edit',link:'developers.update'}]
     });
   }
